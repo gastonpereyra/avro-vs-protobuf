4dc5b399b694064859e0f295a44aa7f6300d317b
gastonpereyra <rgpxen@gmail.com>
2020-12-27 20:00:55 -0300

    Added Dataset and Generators

---
+++ generator/README.md
@@ -0,0 +1,5 @@
+# Y esto ?
+
+Aca van los archivos usados para generar dataset dinamicos como `{{nombre}}.js`
+
+PD: no borren la carpeta
---
+++ generator/perfomance.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const template = {
+    name: 'Person n°',
+    age: 0,
+    isDev: true
+};
+
+const generator = {
+    name: (value, index) => `${value} ${index}`,
+    age: () => Math.ceil(Math.random() * 100),
+    isDev: (value, index, size) => !!(Math.random() * size > size/2)
+}
+
+module.exports = (size = 1000) => Array(size).fill({}).map((item, index) => {
+
+    return Object.entries(template).reduce((itemGenerated, [key, value]) => {
+
+        if(generator[key])
+            itemGenerated[key] = generator[key](value, index, size);
+
+        return itemGenerated;
+
+    }, { ...template });
+});
